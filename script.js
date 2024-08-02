1e341bd2ba5a45a981ba678e14b644b890503e19
PEDROOUSADO <151747830+PEDROOUSADO@users.noreply.github.com>
2024-08-02 09:26:22 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+    const caixaPrinciapal = document.querySelector('.caixa-princiapal');
+    const caixaPergunta = document.querySelector('.caixa-pergunta');
+    const caixaAlternativa = document.querySelector('.caixa-alternativa');
+    const caixaResultado = document.querySelector('.caixa-resultado');
+
+    const pergunta =[
+        {
+            enunciado:"Pergunta 1",
+            alternativa:[
+                "alternativa 1",
+                "Alternativa 2"
+            ]
+        }
+    ]
