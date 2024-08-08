412f3353e3ddf6d03cf2525fa7cae5491285776c
PEDROOUSADO <151747830+PEDROOUSADO@users.noreply.github.com>
2024-08-08 07:43:52 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,9 +6,20 @@
     const pergunta =[
         {
             enunciado:"Pergunta 1",
-            alternativa:[
-                "alternativa 1",
-                "Alternativa 2"
-            ]
+            alternativa:["alternativa 1","Alternativa 2"]
+        },
+        {
+            enunciado:"Pergunta 2",
+            alternativa:["alternativa 1","Alternativa 2"]
+        },
+        {
+            enunciado:"Pergunta 3",
+            alternativa:["alternativa 1","Alternativa 2"]
         }
     ]
+    let posicao = 0;
+    let perguntaAtual;
+
+    function mostraPergunta(){
+        
+    }
