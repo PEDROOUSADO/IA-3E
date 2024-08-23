171ba0d54fc06f77139fe81ce4bd67552c16b4a9
PEDROOUSADO <151747830+PEDROOUSADO@users.noreply.github.com>
2024-08-23 09:24:50 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,25 +1,27 @@
-    const caixaPrinciapal = document.querySelector('.caixa-princiapal');
-    const caixaPergunta = document.querySelector('.caixa-pergunta');
-    const caixaAlternativa = document.querySelector('.caixa-alternativa');
-    const caixaResultado = document.querySelector('.caixa-resultado');
+const caixaPrincipal = document.querySelector('.caixa-principal');
+const caixaPergunta = document.querySelector('.caixa-pergunta');
+const caixaAlternativa = document.querySelector('.caixa-alternativa');
+const caixaResultado = document.querySelector('.caixa-resultado');
 
-    const pergunta =[
-        {
-            enunciado:"Pergunta 1",
-            alternativa:["alternativa 1","Alternativa 2"]
-        },
-        {
-            enunciado:"Pergunta 2",
-            alternativa:["alternativa 1","Alternativa 2"]
-        },
-        {
-            enunciado:"Pergunta 3",
-            alternativa:["alternativa 1","Alternativa 2"]
-        }
-    ]
-    let posicao = 0;
-    let perguntaAtual;
-
-    function mostraPergunta(){
-        
+const perguntas = [//abre a lista de objetos (itens)
+    {//abre o item
+        enunciado:"Pergunta 1",
+        alternativas:["Alternativa 1","Alternativa 2"]
+    },
+    {
+        enunciado:"Pergunta 2",
+        alternativas:["Alternativa 1","Alternativa 2"]
+    },
+    {
+        enunciado:"Pergunta 3",
+        alternativas:["Alternativa 1","Alternativa 2"]
     }
+]
+let posicao = 0;
+let perguntaAtual;
+
+function mostraPergunta(){
+    perguntaAtual = perguntas[posicao];
+    caixaPergunta. textContent = peruntaAtual.enunciado;
+    
+}
