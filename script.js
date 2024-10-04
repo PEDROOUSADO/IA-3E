5670c72b3749ff092ff84b0202ff4c5330af4eef
PEDROOUSADO <151747830+PEDROOUSADO@users.noreply.github.com>
2024-10-04 09:26:35 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,56 +5,72 @@ const caixaResultado = document.querySelector('.caixa-resultado');
 
 const perguntas = [//abre a lista de objetos (itens)
     {//abre o item
-        enunciado:"Você gosta de Inteligência Artificial",
-        alternativa:[{
-            texto:"Sim",
-            afirmação:"Afirmação da Alternativa 1"
+        enunciado: "Você gosta da Inteligência Artificial?",
+        alternativas: [{
+            texto: "Sim",
+            afirmação: "Porque sim"
         },
         {
-        texto:"não",
-        afirmação:"Afirmação da alternativa "
-
+            texto: "Não",
+            afirmação: "Porque Não"
         }
-    ]
-},    
-        {
-            enunciado:"Você usa IA no dia a dia ",
-            alternativa:[{
-                texto:"sim",
-                afirmação:"Porque Sim"
-            },
+        ]
+    },
+    {
+        enunciado: "você usa IA frequentemente",
+        alternativas: [{
+            texto: "Sim",
+            afirmação: "Todos os dias"
+        },
         {
             texto:"Não",
-            afirmação:"Porque não"
-        }   
-            ] 
-            
-        
-        }                                                                                                                                                                                                                                                                                                                                                                                                                   
+            afirmação:"Horrível"
+        }
+        ]
+    },
     {
-        enunciado:"Pergunta 3",
-        alternativas:["Alternativa 1","Alternativa 2"]
+        enunciado: "Você usa IA nos estudos",
+        alternativas: [{
+            texto: "Sim",
+            afirmação: "Nas provas e nas tarefas"
+        },
+        {
+            texto: "Não",
+            afirmação: "não compensa"
+        }
+        ]
     }
 ]
 let posicao = 0;
 let perguntaAtual;
+let respostas = "";
 
 
-function mostraPergunta(){
+function mostraPergunta() {
+    if (posicao>=perguntas.length){
+        mostraResultado();
+        return;
+    }
     perguntaAtual = perguntas[posicao];
-    caixaPergunta. textContent = perguntaAtual.enunciado;
-    mostraAlternativa();
-
+    caixaPergunta.textContent = perguntaAtual.enunciado;
+    caixaAlternativa.textContent = " ";
+    mostraAlternativas();
 }
-function mostraAlternativa(){
-    for (const alternativa of perguntaAtual.alternativa){
-        const botaoAlternativas = document.creatElement("button");
-        botaoAlternativas.textContent = alternativa;
-        botaoAlternativas.addEventListener("click", function(){
-            posicao++;
-        })
+function mostraAlternativas() {
+    for (const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click",  () => respostasSelecionadas(alternativa));
         caixaAlternativa.appendChild(botaoAlternativas);
     }
-
+}
+function respostasSelecionadas(opcaoSelecionadas){
+    const afirmacoes = opcaoSelecionadas.afirmação;
+    respostas = afirmação;
+    posicao++;
+    mostraPergunta();
+}
+function mostraPergunta(){
+    caixaPergunta.textContent = "Daqui a 10 anos ";
 }
 mostraPergunta();
